2eb7b7c1114ecad67f6520f7936a9523ad811a15
Donthireddy Ruthwik Reddy <179065000+ruthwikreddy07@users.noreply.github.com>
2025-07-04 21:53:56 +0530

    Delete Plant-Disease-identification directory

--- Plant-Disease-identification/download_weights.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.applications import MobileNetV2
-
-# This will automatically download the weights correctly and place in .keras/models/
-model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-print("✅ Download complete and verified.")
