a0aa7c537851e82fb8e1bd68d97dc16e1abeb515
Donthireddy Ruthwik Reddy <179065000+ruthwikreddy07@users.noreply.github.com>
2025-07-05 10:25:11 +0530

    Update app.py

--- Plant-Disease-identification/app.py
+++ Plant-Disease-identification/app.py
@@ -14,7 +14,7 @@ from keras.models import load_model
 
 # Get path of current script
 app_dir = Path(__file__).parent
-model_path = app_dir / "plant_disease_model.keras"
+model_path = app_dir / "plant_disease_model.h5"
 
 # Load the model
 model = load_model(model_path)
